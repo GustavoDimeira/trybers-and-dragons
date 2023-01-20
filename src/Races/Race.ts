16ef8f6bb17221a5ce49357a83218efe5ee25b43
Gustavo Dimeira <gustavodimeira1@gmail.com>
2023-01-20 15:58:49 -0400

    req 1

---
+++ src/Races/Race.ts
@@ -0,0 +1,11 @@
+export default abstract class Race {
+  static createdRacesInstances(): void {
+    throw new Error('Not implemented');
+  }
+
+  constructor(private _name: string, private _dexterity: number) { }
+
+  get name(): string { return this._name; }
+  get dexterity(): number { return this._dexterity; }
+  abstract get maxLifePoints(): number;
+}
